5e74b4b2ae6d9ed0563d6b6ba8ad0aa807769517
Aaron Koffel <amkoffel@gmail.com>
2020-11-28 12:36:18 -0800

    added SELECT functionality to cart, orders, and products_orders.

--- webapp.py
+++ webapp.py
@@ -4,10 +4,29 @@ from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    return render_template('index.html')
+@webapp.route('/orders')
+def orderData():
+    print("Fetching and rendering orders web page...")
+    db_connection = connect_to_database()
+    query = "SELECT orderNumber, cartID, finalPrice from orders;"
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('orders.html', rows = result)
 
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
+@webapp.route('/cart')
+def cartData():
+    print("Fetching and rendering cart web page...")
+    db_connection = connect_to_database()
+    query = "SELECT cartID, productID, totalCost, quantity from cart;"
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('cart.html', rows = result)
+
+@webapp.route('/products_orders')
+def products_ordersData():
+    print("Fetching and rendering products_orders web page...")
+    db_connection = connect_to_database()
+    query = "SELECT productID, orderNumber from products_orders;"
+    result = execute_query(db_connection, query).fetchall();
+    print(result)
+    return render_template('products_orders.html', rows = result)
